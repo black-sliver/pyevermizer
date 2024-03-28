158bb5ba07eecd81cd45a57672670a248e03e32c
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-03-28 21:29:01 +0100

    simplify likely macro
    we don't support old gcc anyway

--- src/_evermizer.c
+++ src/_evermizer.c
@@ -7,7 +7,7 @@ namespace _evermizer {
 #define WITH_ASSERT
 #endif
 
-#if defined(__has_builtin) && __has_builtin(__builtin_expect)
+if defined(__GNUC__)
 #define unlikely(expr) __builtin_expect(!!(expr), 0)
 #else
 #define unlikely(expr) (!!(expr))
